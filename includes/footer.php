088556fe8758879832568f205b704ed7e4f9232f
joaoduarte01 <joao_duarte3@estudante.sesisenai.org.br>
2025-10-31 09:15:18 -0300

    commit css e footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
     </main>
     <footer>
         <div class="footer-content">
-            <p>&copy; 2024 Sistema Kanban - Desenvolvimento de Sistemas</p>
+            <p>&copy; 2025 Atividade 14 - Lista de Revisão de CRUD - Kanban </p>
         </div>
     </footer>
 </body>
