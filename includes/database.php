bb1f781d782d4899aff7c4908fed6bc741d1192f
joaoduarte01 <joao_duarte3@estudante.sesisenai.org.br>
2025-10-31 09:10:09 -0300

    database resolvido

--- includes/database.php
+++ includes/database.php
@@ -2,11 +2,14 @@
 $host = 'localhost';
 $dbname = 'kanban_sistema';
 $username = 'root';
-$password = '';
+// Tente estas senhas comuns no XAMPP
+$password = ''; // Senha vazia (mais comum)
+// $password = 'root'; // Alternativa comum
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Conexão bem-sucedida!"; // Descomente para testar
 } catch(PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
